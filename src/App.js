e3694bc764453cce47f50973f8ac62ff62b8d499
takanassyi <takanassyi@gmail.com>
2020-11-17 21:00:47 +0900

    create clas component and function component

--- src/App.js
+++ src/App.js
@@ -2,33 +2,46 @@
 //   return (<div><p>Hello, world!</p></div>);
 // }
 import React, { Component } from 'react';
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => { console.log("I am clicked!") }} />
-      </React.Fragment>
-    );
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => { console.log("I am clicked!") }} />
+//       </React.Fragment>
+//     );
 
-    // return (
-    //   <div>
-    //     <label htmlFor="bar">bar</label>
-    //     <input type="text" onChange={() => { console.log("I am clicked!") }} />
-    //   </div>
-    // );
+//     // return (
+//     //   <div>
+//     //     <label htmlFor="bar">bar</label>
+//     //     <input type="text" onChange={() => { console.log("I am clicked!") }} />
+//     //   </div>
+//     // );
 
-    // return <input type="text" onClick={() => { console.log("Hi!") }} />
+//     // return <input type="text" onClick={() => { console.log("Hi!") }} />
 
-    // return <input type="text" onClick={() => { console.log("I am clicked") }} />
+//     // return <input type="text" onClick={() => { console.log("I am clicked") }} />
 
-    // const greeting = "Hi, Tom!";
-    // const dom = <h1 className="foo">{greeting}</h1>
-    // return dom;
+//     // const greeting = "Hi, Tom!";
+//     // const dom = <h1 className="foo">{greeting}</h1>
+//     // return dom;
 
-    //return <h1>Hello, world!</h1>;
-    //return null;
-  }
+//     //return <h1>Hello, world!</h1>;
+//     //return null;
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
 
+const Cat = () => {
+  return <div>Meow!</div>
+}
 export default App;
